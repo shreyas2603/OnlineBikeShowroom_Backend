31ba8a9fc73e928fba373a26128e423bcb82cd58
Shreyas <shreyasrb2003@gmail.com>
2023-11-18 18:16:21 +0530

    fourth commit

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const Bike = require('../models/bike');
+const Razorpay = require('razorpay');
 
 // Get all bikes
 router.get('/bikes', async (req, res) => {
@@ -41,6 +42,8 @@ router.put('/bikes/:id', async (req, res) => {
   }
 });
 
+
+//Delete a bike
 router.delete('/bikes/:id', async (req,res) => {
   const bikeId = req.params.id;
 
@@ -82,6 +85,70 @@ router.post('/bikes', async (req, res) => {
     res.status(500).json({ error: 'An error occurred' });
   }
 });
+
+
+//Payment
+
+const razorpay = new Razorpay({
+  key_id:'rzp_test_8NRMErRVMXFiiA',
+  key_secret:'0rKYqZY5b6FiDghcGMX1DARV',
+});
+
+router.post('/orders', async (req, res) => {
+  try {
+    const { amount } = req.body;
+
+    const orderOptions = {
+      amount, 
+      currency: 'INR',
+      receipt: 'order_rcptid_11',
+      payment_capture: 1,
+    };
+
+    razorpay.orders.create(orderOptions, (error, order) => {
+      if (error) {
+        console.error('Error creating Razorpay order:', error);
+        return res.status(500).json({ error: 'Error creating Razorpay order' });
+      }
+
+      res.json({
+        orderId: order.id,
+        options: {
+          key: razorpay.key_id,
+          amount: order.amount,
+          currency: order.currency,
+          order_id: order.id,
+          name: 'Your App Name',
+          description: 'Payment for Bike Purchase',
+          prefill: {
+            name: 'User Name',
+            email: 'user@example.com',
+            contact: '9876543210',
+          },
+          theme: {
+            color: '#F37254',
+          },
+          callback_url: "http://localhost:4000/api/success", // Update the server-side route
+ // Redirect to the success page
+        },
+      });
+    });
+  } catch (error) {
+    console.error('Error creating Razorpay order:', error);
+    res.status(500).json({ error: 'Error creating Razorpay order' });
+  }
+});
+
+
+// Add this route to handle the callback_url
+// routes/api.js
+router.post('/success', (req, res) => {
+  // Handle the POST request for the success page
+  // You can perform any necessary actions here
+  //res.json({ message: 'Payment successful! Redirect to success page.' });
+  res.redirect('http://localhost:3000/#/success'); // Redirect to the frontend success page
+});
+
 
 
 module.exports = router;
